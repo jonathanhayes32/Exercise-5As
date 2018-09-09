7e685a7cd6a549ef920d28a4f592d672cb0f99c9
Jonathan Hayes <jonathan.hayes32@outlook.com>
2018-09-08 20:08:05 -0400

    git commit(Add) to command line

---
+++ main part 2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main_part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //build a farm
+            Mouse Cheryll = new Mouse();
+            Chicken Martha = new Chicken();
+            Goat Spot = new Goat();
+            Sheep Beatrice = new Sheep();
+
+            Console.WriteLine("Hello and welcome to my farm!");
+            Console.WriteLine("Let me introduce you to our animals");
+            Console.WriteLine();
+        }
+
+        private class Mouse
+        {
+            public Mouse()
+            {
+            }
+        }
+
+        private class Chicken
+        {
+            public Chicken()
+            {
+            }
+        }
+
+        private class Goat
+        {
+            public Goat()
+            {
+            }
+        }
+
+        private class Sheep
+        {
+            public Sheep()
+            {
+            }
+        }
+    }
+}
